82fcc6fc4714798df56840c1b87a0a7bb884a159
tochau2016 <tochau2016@gmail.com>
2021-07-07 23:57:20 +0700

    update css

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSearch() {
+    var x = document.getElementById("searchBar");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
