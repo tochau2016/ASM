14d78bb825ffbe7edc858cf4da2c885748a40d9c
tochau2016 <tochau2016@gmail.com>
2021-07-10 00:19:04 +0700

    done login + fix some function

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-function showSearch() {
-    var x = document.getElementById("searchBar");
-    if (x.style.display !== "none") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+$(document).ready(function () {
+    $('.home-slick').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('#showSearchBar').click(function () {
+        $('.searchBar').toggle();
+    })
+})
+
+function showChangeProfile(hide, show) {
+    document.getElementById(hide).style.display = "none";
+    document.getElementById(show).style.display = "block";
 }
